94e32c33b37535bda454194813902f20e9b0d1f3
Shane Upchurch <shaneupchurch@gmail.com>
2020-09-29 18:34:18 -0500

    adds function app

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 // import functions and grab DOM elements
+
+import {compareNumbers} from "./mathUtils.js"
+
 const button = document.getElementById('button');
 const guess = document.getElementById('user-input');
-const triesRemaining = document.getElementById('guesses');
+const attemptsLeft = document.getElementById('guesses');
 const hiOrLow = document.getElementById('high-low');
 const results = document.getElementById('results');
 
 const randomNum = Math.floor(Math.random()*20) + 1;
 console.log(randomNum);
 
-
 // initialize state
-let tries = 0;
+let attempts = 4;
 
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
-    const guessMax = 4;
-    tries++;
-    const numGuess = guess.value;
-    
-        if (numGuess > randomNum){
-          return hiOrLow.textContent = 'Too high!';
-        }
+    attempts--;
+    const numGuess = guess.valueAsNumber;
+    if (attempts === 0 ){
+        attemptsLeft.textContent = "Game Over!"
     }
+
+    const gameResult = compareNumbers(numGuess, randomNum);
+console.log(gameResult);
+
+    if (gameResult === 1){
+        hiOrLow.textContent = "Too high!";
+    }
+    if (gameResult === -1){
+        hiOrLow.textContent = "Too low!";
+    } else if (
+        gameResult === 0){
+        results.textContent = "You got it!";
+    }
+    
+    
+    
+        
 
 })
 

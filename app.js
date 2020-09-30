9212540389ecb0aa428b70ff4878d19bac07f2cf
Shane Upchurch <shaneupchurch@gmail.com>
2020-09-29 20:12:07 -0500

    disables button on game end and adds image

--- app.js
+++ app.js
@@ -17,27 +17,29 @@ let attempts = 4;
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
     attempts--;
+    attemptsLeft.textContent = attempts;
+    
     const numGuess = guess.valueAsNumber;
+    
     if (attempts === 0 ){
-        attemptsLeft.textContent = "Game Over!"
+        attemptsLeft.textContent = "GAME OVER!"
+        button.disabled = true;
     }
 
     const gameResult = compareNumbers(numGuess, randomNum);
 console.log(gameResult);
 
     if (gameResult === 1){
-        hiOrLow.textContent = "Too high!";
+        hiOrLow.textContent = "Whoa! Take a little off homie.";
     }
     if (gameResult === -1){
-        hiOrLow.textContent = "Too low!";
+        hiOrLow.textContent = "Nope, it's higher than that!";
     } else if (
         gameResult === 0){
-        results.textContent = "You got it!";
+        results.textContent = "BINGO!";
+        hiOrLow.textContent = "";
+        button.disabled = true;
+        return;
     }
-    
-    
-    
-        
-
 })
 
